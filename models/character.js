fd6e4986fe34e6b6d82670bf54154031c74ccabd
Ari Horowitz <ariehh@comcast.net>
2020-03-05 19:23:10 -0800

    character.js attributes completed

--- models/character.js
+++ models/character.js
@@ -1,12 +1,159 @@
 //Dependencies
-const Sequelize = require('sequelize');
-const db = require('../config/connection');
-
+const Sequelize = require("sequelize");
+const db = require("../config/connection");
 
 //Model
-const Character = db.define('character', {
-  //fields will go here once we figure out what they are.  
-})
+const Character = db.define("character", {
+  character_name: {
+    type: datatype.STRING,
+  },
+  class: {
+    type: datatype.STRING,
+  }
+  level: {
+    type: datatype.INT,
+  }
+  background: {
+    type: datatype.STRING,
+  }
+  player_name: {
+    type: datatype.STRING,
+  }
+  faction: {
+    type: datatype.STRING,
+  }
+  race: {
+    type: datatype.STRING,
+  }
+  alignment: {
+    type: datatype.STRING,
+  }
+  exp: {
+    type: datatype.INT,
+  }
+  inspiration: {
+type: datatype.STRING,
+  }
+  strength: {
+    type: datatype.INT,
+  }
+  dexterity: {
+    type: datatype.INT,
+  }
+  constitution: {
+    type: datatype.INT,
+  }
+  intelligence: {
+    type: datatype.INT,
+  }
+  wisdom: {
+    type: datatype.INT,
+  }
+  charisma: {
+    type: datatype.INT,
+  }
+  strength_st{
+    type: datatype.INT,
+  }
+  dexterity_st{
+    type: datatype.INT,
 
+  }
+  constitution_st:{
+    type: datatype.INT,
+
+  }
+  intelligence_st:{
+    type: datatype.INT,
+
+  }
+  wisdom_st:{
+    type: datatype.INT,
+
+  }
+  inspiration: {
+    type: datatype.STRING,
+  }
+  armor_class: {
+    type: datatype.STRING,
+
+  }
+  initiative: {
+    type: datatype.STRING,
+
+  }
+  speech:{
+    type: datatype.STRING,
+
+  }
+  current_hp: {
+    type: datatype.STRING,
+
+  }
+  temp_hp: {
+    type: datatype.STRING,
+
+  }
+  personality_traits: {
+    type: datatype.STRING,
+
+  }
+  ideals: {
+    type: datatype.STRING,
+
+  }
+  bonds: {
+    type: datatype.STRING,
+
+  }
+  flaws: {
+    type: datatype.STRING,
+
+  }
+  attacks_and_spellcasting: {
+    type: datatype.STRING,
+
+  }
+  passive_wisdom: {
+    type: datatype.STRING,
+
+  }
+  equipment: {
+    type: datatype.STRING,
+
+  }
+  features_and_traits: {
+    type: datatype.STRING,
+
+  }
+  other_proficiencies_and_languages: {
+    type: datatype.STRING,
+
+  }
+  age: {
+    type: datatype.INT,
+  }
+  height: {
+    type: datatype.INT,
+  }
+  weight: {
+    type: datatype.INT,
+  }
+  eyes: {
+    type: datatype.STRING,
+
+  }
+  skin: {
+    type: datatype.STRING,
+
+  }
+  hair: {
+    type: datatype.STRING,
+
+  }
+  
+
+  //fields will go here once we figure out what they are.
+});
 
 module.exports = Character;
