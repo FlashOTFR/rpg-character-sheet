e7e355e94e3cffabda56afe76a7364a0607249f4
Issouf Kiema <walybi03@yahoo.com>
2020-03-06 05:16:46 -0800

    modified server.js and we can add more dependencies as we go. also created routes folder with api-routes.js and html-routes.js. A view folder with a main handlebar also was created.

--- server.js
+++ server.js
@@ -1,23 +1,65 @@
-//Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
+'use strict';
 
-//Created Server
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require(`express`);
+
+// Sets up the Express App
+// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Get LANDING PAGE
-app.get('/', (req, res) => res.send('LANDING PAGE HERE'));
+// Requiring our models for syncing
+const db = require(`./models`);
 
-//Database
-const db = require('./config/connection');
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Database connection test
-db.authenticate().then(() => console.log('Database Connected...')).catch(err => console.log('Error' + err));
+// Static directory
+app.use(express.static(`public`));
+
+// Set Handlebars.
+const expressHandlebars = require('express-handlebars');
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Routes
+// =============================================================
+require(`./routes/api-routes.js`)(app);
+require(`./routes/html-routes.js`)(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`App listening on http://localhost:${PORT}`)
+  );
+});
 
 
-app.use('/characters', require('./routes/characters'));
-//Server listener
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+// //Dependencies
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+
+// //Sets up the Express App
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// //Get LANDING PAGE
+// app.get('/', (req, res) => res.send('LANDING PAGE HERE'));
+
+// //Database
+// const db = require('./config/connection');
+
+// //Database connection test
+// db.authenticate().then(() => console.log('Database Connected...')).catch(err => console.log('Error' + err));
+
+
+// app.use('/characters', require('./routes/characters'));
+// //Server listener
+// app.listen(PORT, console.log(`Server started on port ${PORT}`));
