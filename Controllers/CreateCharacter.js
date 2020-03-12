8219bc6645779883f51263060fcbc789c7e23723
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-03-11 19:33:01 -0700

    edited tests

---
+++ Controllers/CreateCharacter.js
@@ -0,0 +1,24 @@
+class CreateCharacter {
+    constructor (name, type, background){
+        if (!name){
+            throw new Error("Please enter a name for your character.")
+
+        }
+        if (!type){
+            throw new Error("Please enter a character class-type.")
+        }
+        if (!background){
+            throw new Error("please enter a character background story.")
+        
+        }
+        this.name = Name;
+        this.type = Class-Type;
+        this.background = Background;
+        
+    }
+
+
+
+}
+module.exports = CreateCharacter;
+
