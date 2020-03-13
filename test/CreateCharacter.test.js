acff2f8ea92bb5d76935ed1f4c36bf0291f6f96a
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-03-12 20:32:39 -0700

    edited tests

--- test/CreateCharacter.test.js
+++ test/CreateCharacter.test.js
@@ -1,28 +1,28 @@
-// 'use strict';
+'use strict';
 
-// const Character = require("../Controllers/CreateCharacter");
+const Character = require("../Controllers/CreateCharacter");
 
-// describe("CreateCharacter", () => {
-//     describe('Initialization', () => {
-//         it('should create a characer with a name which is a string', () => {
-//             const character = new Character("Magus", "fighter-mage", "bastard prince");
+describe("CreateCharacter", () => {
+    describe('Initialization', () => {
+        it('should create a characer with a name which is a string', () => {
+            const character = new Character("Magus", "fighter-mage", "bastard prince");
 
-//             expect(character.name).toEqual("Magus");
-//         });
+            expect(character.name).toEqual("Magus");
+        });
 
-//     });
-//     it("should create character class-type.", () => {
-//         const character = new Character("Magus", "fighter-mage", "bastard prince");
+    });
+    it("should create character class-type.", () => {
+        const character = new Character("Magus", "fighter-mage", "bastard prince");
 
-//         expect(character.type).toEqual("fighter-mage");
-//      });
-//      it("should create character background story", () => {
-//         const character = new Character("Magus", "fighter-mage", "bastard prince");
+        expect(character.type).toEqual("fighter-mage");
+     });
+     it("should create character background story", () => {
+        const character = new Character("Magus", "fighter-mage", "bastard prince");
 
-//         expect(character.background).toEqual("bastard prince");
+        expect(character.background).toEqual("bastard prince");
 
 
      
-// });
-// });
+});
+});
 
