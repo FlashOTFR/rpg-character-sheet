809793dea1758b0bd1719662da46e903a0bed59f
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-03-14 12:49:39 -0700

    deleted empty file

--- test/CreateCharacter.test.js
+++ test/CreateCharacter.test.js
@@ -5,27 +5,36 @@ const Character = require("../Controllers/CreateCharacter");
 describe("CreateCharacter", () => {
     describe('Initialization', () => {
         it('should create a characer with a name which is a string', () => {
-            const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
+            const character = new Character("Magus", "fighter-mage", "bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail",);
 
             expect(character.name).toEqual("Magus");
         });
 
     });
     it("should create character class-type.", () => {
-        const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
+        const character = new Character("Magus", "fighter-mage", "bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail",);
 
         expect(character.type).toEqual("fighter-mage");
     });
 
-    it("should create character level.", () => {
-        const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
+    it("should create character background.", () => {
+        const character = new Character("Magus", "fighter-mage", "bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail",);
 
-        expect(character.level).toEqual ("12");
+        expect(character.background).toEqual("bastard prince");
     });
+
+    // it("should create character faction.", () => {
+    //     const character = new Character("Magus", "fighter-mage", "bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail",);
+
+    //     expect(character.faction).toEqual("black magic");
+    // });
     
+   
     
     
  
-    });
+    })
+
+    
 
 
