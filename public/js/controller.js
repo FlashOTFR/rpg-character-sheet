6045b82bad2b64d86cefc4ac1f84a2773fed1758
Randall Burgess <randallallenburgess@gmail.com>
2020-03-13 19:07:26 -0700

    changed placeholders to values in text fields for singlechar, working on update request.

--- public/js/controller.js
+++ public/js/controller.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
   $('.delBtn').on("click", deleteCharacter);
   $('.shoBtn').on('click', singleChar);
   $('#create-button').on('click', createCharacter);
+  $('#update-button').on('click', console.log('update button pressed'));
 
   //DELETE function
   
@@ -105,4 +106,79 @@ $( document ).ready(function() {
       console.log('character created');
       location.reload();
     }
+});
+
+function updateChar(event){
+  event.preventDefault();
+  console.log('update button pressed');
+  const id = $(this).data(`id`);
+  console.log('this is your id ' + id);
+  const character = {
+    name: $('#character-name').val(),
+    class: $('#character-class').val(),
+    level: $('#character-level').val(),
+    background: $('#character-background').val(),
+    player_name: $('#character-player_name').val(),
+    faction: $('#character-faction').val(),
+    race: $('#character-race').val(),
+    alignment: $('#character-alignment').val(),
+    exp: $('#character-exp').val(),
+    inspiration: $('#character-inspiration').val(),
+    strength: $('#character-strength').val(),
+    dexterity: $('#character-dexterity').val(),
+    constitution: $('#character-constitution').val(),
+    intelligence: $('#character-intelligence').val(),
+    wisdom: $('#character-wisdom').val(),
+    charisma: $('#character-charisma').val(),
+    strength_st:$('#character-strength_st').val(),
+    dexterity_st: $('#character-dexterity_st').val(),
+    constitution_st: $('#character-constitution_st').val(),
+    intelligence_st: $('#character-intelligence_st').val(),
+    wisdom_st: $('#character-wisdom_st').val(),
+    charisma_st: $('#character-charisma_st').val(),
+    armor_class: $('#character-armor_class').val(),
+    initiative: $('#character-initiative').val(),
+    speed: $('#character-speed').val(),
+    current_hp: $('#character-current_hp').val(),
+    temp_hp: $('#character-temp_hp').val(),
+    personality_traits: $('#character-personality_traits').val(),
+    ideals: $('#character-ideals').val(),
+    bonds: $('#character-bonds').val(),
+    flaws: $('#character-flaws').val(),
+    attacks_and_spellcasting: $('#character-attacks_and_spellcasting').val(),
+    passive_wisdom: $('#character-passive_wisdom').val(),
+    equipment: $('#character-equipment').val(),
+    features_and_traits: $('#character-features_and_traits').val(),
+    other_proficiencies_and_languages: $('#character-other_proficiences_and_languages').val(),
+    age: $('#character-age').val(),
+    height: $('#character-height').val(),
+    weight: $('#character-weight').val(),
+    eyes: $('#character-eyes').val(),
+    skin: $('#character-skin').val(),
+    hair: $('#character-hair').val(),
+    acrobatics: $('#character-acrobatics').val(),
+    animal_handling: $('#character-animal_handling').val(),
+    arcana: $('#character-arcana').val(),
+    athletics: $('#character-athletics').val(),
+    deception: $('#character-deception').val(),
+    history: $('#character-history').val(),
+    insight: $('#character-insight').val(),
+    intimidation: $('#character-intimidation').val(),
+    investigation: $('#character-investigation').val(),
+    medicine: $('#character-medicine').val(),
+    nature: $('#character-nature').val(),
+    perception: $('#character-perception').val(),
+    performance: $('#character-performance').val(),
+    persuasion: $('#character-persuasion').val(),
+    religion: $('#character-religion').val(),
+    sleight_of_hand: $('#character-sleight_of_hand').val(),
+    stealth: $('#character-stealth').val(),
+    survival: $('#character-survival').val(),
+      
+  };
+  console.log(character);
+  $.put(`/api/yourchar/`, character);
+  console.log('character created');
+  location.reload();
+}
 });
