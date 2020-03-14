2ffa8103ad8abbdfbd9de625f89044c8b86639b7
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-03-13 22:57:56 -0700

    added test

--- test/CreateCharacter.test.js
+++ test/CreateCharacter.test.js
@@ -5,24 +5,27 @@ const Character = require("../Controllers/CreateCharacter");
 describe("CreateCharacter", () => {
     describe('Initialization', () => {
         it('should create a characer with a name which is a string', () => {
-            const character = new Character("Magus", "fighter-mage", "bastard prince");
+            const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
 
             expect(character.name).toEqual("Magus");
         });
 
     });
     it("should create character class-type.", () => {
-        const character = new Character("Magus", "fighter-mage", "bastard prince");
+        const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
 
         expect(character.type).toEqual("fighter-mage");
-     });
-     it("should create character background story", () => {
-        const character = new Character("Magus", "fighter-mage", "bastard prince");
+    });
 
-        expect(character.background).toEqual("bastard prince");
+    it("should create character level.", () => {
+        const character = new Character("Magus", "fighter-mage", "12","bastard prince", "black magic", "Vaughn", "loner", "halfing", "chaotic-neutral", "9,000", "balance", "lots", "real fast", "sickly", "meh", "also meh", "slightly charming", "light", "keep the peace", "fast snail", "5");
 
+        expect(character.level).toEqual ("12");
+    });
+    
+    
+    
+ 
+    });
 
-     
-});
-});
 
